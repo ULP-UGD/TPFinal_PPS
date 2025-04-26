fb5694be4f210fb5bc7294824c9967e7c82e92aa
JNZader <127463636+JNZader@users.noreply.github.com>
2025-04-26 14:52:00 -0300

    feat(front): crear pagina de inicio
    Crear página /home estática con estilos de Tailwind y componentes de shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Registro from './pages/Registro';
+import Home from './pages/Home';
 
 export default function App() {
   return (
     <div className="gry">
       <Routes>
-        <Route path="/login" element={<Login />}/>
-        <Route path='/registro' element={<Registro/>}/>
-        <Route path="/" element={<h1 className="text-3xl font-bold text-center pt-10">Bienvenido</h1>} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/registro' element={<Registro />} />
+        <Route path='/home' element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
